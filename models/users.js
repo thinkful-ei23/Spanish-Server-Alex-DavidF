955c959dc46902039fcebc469eb16b63cc434c7e
David Folks <aldwyn.wot@gmail.com>
2018-10-15 12:11:04 -0600

    added JWT auth

---
+++ models/users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.methods.validatePassword = function(password) {
+  return password === this.password;
+};
+
+userSchema.set('toObject', {
+  virtuals: true,
+  versionKey: false,
+  transform: (doc, ret) => {
+    delete ret._id;
+    delete ret.password;
+  }
+});
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+};
+
+module.exports = mongoose.model('User', userSchema);
