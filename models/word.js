7fe238ca5b93a72df89ecc3e8f23da1f9b72b4d5
Alex Puhl <apuhlswimmer@hotmail.com>
2018-10-17 12:46:02 -0600

    removed next from word schema

--- models/word.js
+++ models/word.js
@@ -5,8 +5,7 @@ const mongoose = require('mongoose');
 const wordSchema = new mongoose.Schema({
   spanish: { type: String, required: true, unique: true },
   english: { type: String, required: true },
-  mVal: { type: Number, default: 1 },
-  next: { type: Number }
+  mVal: { type: Number, default: 1 }
 });
 
 wordSchema.set('toObject', {
